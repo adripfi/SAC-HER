0ce76bca29bccb94d9a4dbd009a0d3dfae81c943
Adrian <a.pfisterer@campus.tu-berlin.de>
2021-07-11 18:23:49 +0200

    humanoid converged

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from replay_buffer import ReplayBuffer
 from utils import LogUtil, test_agent
 
 
-max_episode = int(1e6)
 hidden_size = 256
 alpha = 0.2
 auto_entropy = True
@@ -16,12 +15,12 @@ tau = 0.005
 gamma = 0.99
 lr = 3e-4
 updates_per_step = 1
-max_steps = 1e6
+max_steps = int(1e6)
 start_random = 2000
 eval_interval = 20  # episodes
 seed = None
 
-env_id = "Hopper-v2"
+env_id = "Humanoid-v2"
 env = gym.make(env_id)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device {device} \n")
@@ -41,9 +40,9 @@ def main():
     memory = ReplayBuffer(buffer_size, state_size, action_size, device)
     log = LogUtil(env_id)
 
-    total_steps = 0
-    total_updates = 0
-    for episode in range(max_episode):
+    updates = 0
+    episodes = 0
+    for total_steps in range(max_steps):
         episode_reward = 0
         episode_steps = 0
         done = False
@@ -58,15 +57,15 @@ def main():
                 action = agent.sample(state)
 
             if len(memory) > batch_size:
+                # update agent's weights
                 for i in range(updates_per_step):
                     q1_loss, q2_loss, pi_loss, alpha_loss = agent.update_parameters(memory, batch_size)
-                    log.loss(q1_loss, q2_loss, pi_loss, alpha_loss, agent.alpha, total_updates)
-                    total_updates += 1
+                    log.loss(q1_loss, q2_loss, pi_loss, alpha_loss, agent.alpha, updates)
+                    updates += 1
 
             next_state, reward, done, _ = env.step(action)
 
             episode_steps += 1
-            total_steps += 1
             episode_reward += reward
 
             # allow infinite bootstrapping when the episode terminated due to time limit
@@ -76,25 +75,19 @@ def main():
 
             state = next_state
 
+        episodes += 1
         log.reward(total_steps, episode_reward, "train")
 
-        if not episode % eval_interval:
+        if not episodes % eval_interval:
             # evaluate current policy
             eval_reward, eval_steps = test_agent(agent, env)
 
-            print(f"Episodes trained: {episode}, Eval Reward: {eval_reward}, Episode Steps: {eval_steps}")
-            log.reward(episode, eval_reward, "eval")
+            print(f"Episodes trained: {episodes}, Eval Reward: {eval_reward}, Episode Steps: {eval_steps}")
+            log.reward(episodes, eval_reward, "eval")
             log.save_checkpoints(agent, memory)
-
-        if total_steps > max_steps:
-            break
 
     env.close()
 
-    return agent
-
-
-
 
 if __name__ == "__main__":
-    agent = main()
+    main()
