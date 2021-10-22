ec294df3c75d749fb3beef211ad8f37303639a36
Adrian <a.pfisterer@campus.tu-berlin.de>
2021-10-22 18:43:17 +0200

    tuned for fetch pick and place

--- main.py
+++ main.py
@@ -5,28 +5,31 @@ import torch
 import numpy as np
 from agent import Agent
 from replay_buffer import ReplayBuffer
-from utils import LogUtil, test_agent
+from utils import LogUtil, eval
 from her import HindsightReplayBuffer
 # os.environ["LD_LIBRARY_PATH"] = "$LD_LIBRARY_PATH:/home/adrian/.mujoco/mujoco200/bin"
+import random
 
 
 
 hidden_size = 512
-alpha = 0.1
+alpha = 1.
 auto_entropy = True
 buffer_size = int(1e6)
-batch_size = 128
-tau = 0.005
+batch_size = 1024
+tau = 0.0005
 gamma = 0.95
-lr = 0.0006
+lr = 0.0005
 updates_per_step = 1
-max_steps = int(1e6)
-max_episodes = int(1e6)
-start_random = 1000
-eval_interval = 20  # episodes
-seed = None
+max_steps = int(2e6)
+# max_episodes = int(1e6)
+start_random = int(1e4)
+start_learning = int(1e4)
+eval_interval = 100  # episodes
+seed = 8
 
 env_id = "FetchPickAndPlace-v1"
+
 env = gym.make(env_id)
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(f"Using device {device} \n")
@@ -34,11 +37,15 @@ print(f"Using device {device} \n")
 
 if seed is not None:
     torch.manual_seed(seed)
+    if device == "cuda":
+        torch.cuda.manual_seed(seed)
     np.random.seed(seed)
     env.seed(seed)
+    random.seed(seed)
 
 
-def main():
+
+def train():
     # state_size = env.observation_space.shape[0]
     # action_size = env.action_space.shape[0]
     # goal_des_size = 0
@@ -47,23 +54,23 @@ def main():
     goal_des_size = env.observation_space['desired_goal'].shape[0]
     # achieved_goal_shape = env.observation_space[prefix + 'achieved_goal'].shape
     action_size = env.action_space.shape[0]
-    print("action", action_size, "state", state_size, "goal_des", goal_des_size)
+    # print("action", action_size, "state", state_size, "goal_des", goal_des_size)
 
     agent = Agent(state_size, action_size, goal_des_size, action_high, device, lr, hidden_size, gamma, tau, alpha, auto_entropy)
     # TODO: clean memory init i.e. remove redundant action, state and des goal dim. computation
     memory = HindsightReplayBuffer(False, env, 1, env.observation_space, env.action_space, buffer_size, device)
     log = LogUtil(env_id)
 
-    updates = 0
-    for episodes in range(max_episodes):
+    total_steps = 0
+    episodes = 0
+    while total_steps < max_steps:
         episode_reward = 0
         episode_steps = 0
-        total_steps = 0
         done = False
         state_dict = env.reset()
         state = state_dict["observation"]
         goal_desired = state_dict["desired_goal"]
-        successes = np.zeros(eval_interval)
+        # successes = np.zeros(eval_interval)
 
         while not done:
             if total_steps < start_random:
@@ -73,12 +80,12 @@ def main():
                 # sample action from current policy
                 action = agent.sample(state, goal_desired)
 
-            if len(memory) > batch_size:
+            # print(f"len meme {len(memory)}, steps {episode_steps}, start {start_learning}, bool {len(memory) > batch_size and total_steps > start_learning}")
+            if total_steps > start_learning:
                 # update agent's weights
                 for i in range(updates_per_step):
                     q1_loss, q2_loss, pi_loss, alpha_loss = agent.update_parameters(memory, batch_size)
-                    log.loss(q1_loss, q2_loss, pi_loss, alpha_loss, agent.alpha, updates)
-                    updates += 1
+                    log.loss(q1_loss, q2_loss, pi_loss, alpha_loss, agent.alpha, total_steps)
 
             state_dict, reward, done, info = env.step(action)
             next_state = state_dict["observation"]
@@ -88,7 +95,7 @@ def main():
             total_steps += 1
             episode_reward += reward
 
-            # allow infinite bootstrapping when the episode terminated due to time limit
+            # allow infinite bootstrapping when the episode terminated due to step limit
             # TODO: check for done max
             done = float(done)
             done_no_max = 0 if episode_steps + 1 == env.spec.max_episode_steps else done
@@ -99,21 +106,22 @@ def main():
             state = next_state
             goal_desired = state_dict["desired_goal"]
 
-        log.reward(updates, episode_reward, "train")
-        successes[episodes % eval_interval] = float(info["is_success"])
+        episodes += 1
+        log.reward(total_steps, episode_reward, "train")
+        # successes[episodes % eval_interval] = float(info["is_success"])
 
-        if not episodes % eval_interval:
+        if episodes % eval_interval == 0:
             # evaluate current policy
-            eval_reward, eval_steps = test_agent(agent, env)
+            eval_reward, eval_steps, success = eval(agent, env)
 
-            successes_mean = np.mean(successes)
-            print(f"Episodes trained: {episodes}, Eval Reward: {eval_reward}, Succ: {successes_mean},  Episode Steps: {eval_steps}")
-            log.reward(episodes, eval_reward, "eval")
-            log.reward(episodes, successes_mean, "success")
+            # successes_mean = np.mean(successes)
+            print(f"Steps {total_steps}, Episodes trained: {episodes}, Eval Reward: {eval_reward}, Succ: {success},  Episode Steps: {eval_steps}")
+            log.reward(total_steps, eval_reward, "eval")
+            log.reward(total_steps, success, "success")
             log.save_checkpoints(agent, memory)
 
     env.close()
 
 
 if __name__ == "__main__":
-    main()
+    train()
--- utils.py
+++ utils.py
@@ -54,7 +54,7 @@ class LogUtil:
         agent.policy.load_state_dict(torch.load(os.path.join(path, "actor")))
 
 
-def test_agent(agent, env, render=False):
+def eval(agent, env, render=False):
     state_dict = env.reset()
     state = state_dict["observation"]
     goal_desired = state_dict["desired_goal"]
@@ -62,12 +62,11 @@ def test_agent(agent, env, render=False):
     steps = 0
     done = False
     while not done:
-        if render: env.render()
+        if render:
+            env.render()
         action = agent.sample(state, goal_desired)
-        # next_state, reward, done, _ = env.step(action)
         state_dict, reward, done, info = env.step(action)
         next_state = state_dict["observation"]
-        # goal_achieved = state_dict["achieved_goal"]
 
         total_reward += reward
         steps += 1
@@ -75,64 +74,58 @@ def test_agent(agent, env, render=False):
 
     env.close()
 
-    return total_reward, steps
+    return total_reward, steps, float(info["is_success"])
 
-class EnvWrapper:
-    def __init__(self, env):
-        self.env = env
-        
-class TanhTransform(pyd.transforms.Transform):
-    domain = pyd.constraints.real
-    codomain = pyd.constraints.interval(-1.0, 1.0)
-    bijective = True
-    sign = +1
-
-    def __init__(self, cache_size=1):
-        super().__init__(cache_size=cache_size)
-
-    @staticmethod
-    def atanh(x):
-        return 0.5 * (x.log1p() - (-x).log1p())
-
-    def __eq__(self, other):
-        return isinstance(other, TanhTransform)
-
-    def _call(self, x):
-        return x.tanh()
-
-    def _inverse(self, y):
-        # We do not clamp to the boundary here as it may degrade the performance of certain algorithms.
-        # one should use `cache_size=1` instead
-        return self.atanh(y)
-
-    def log_abs_det_jacobian(self, x, y):
-        # We use a formula that is more numerically stable, see details in the following link
-        # https://github.com/tensorflow/probability/commit/ef6bb176e0ebd1cf6e25c6b5cecdd2428c22963f#diff-e120f70e92e6741bca649f04fcd907b7
-        return 2. * (math.log(2.) - x - F.softplus(-2. * x))
-
-class SquashedNormal(pyd.transformed_distribution.TransformedDistribution):
-    def __init__(self, loc, scale):
-        self.loc = loc
-        self.scale = scale
-
-        self.base_dist = pyd.Normal(loc, scale)
-        transforms = [TanhTransform()]
-        super().__init__(self.base_dist, transforms)
-
-    @property
-    def mean(self):
-        mu = self.loc
-        for tr in self.transforms:
-            mu = tr(mu)
-        return mu
-
-
+# class EnvWrapper:
+#     def __init__(self, env):
+#         self.env = env
+#
+# class TanhTransform(pyd.transforms.Transform):
+#     domain = pyd.constraints.real
+#     codomain = pyd.constraints.interval(-1.0, 1.0)
+#     bijective = True
+#     sign = +1
+#
+#     def __init__(self, cache_size=1):
+#         super().__init__(cache_size=cache_size)
+#
+#     @staticmethod
+#     def atanh(x):
+#         return 0.5 * (x.log1p() - (-x).log1p())
+#
+#     def __eq__(self, other):
+#         return isinstance(other, TanhTransform)
+#
+#     def _call(self, x):
+#         return x.tanh()
+#
+#     def _inverse(self, y):
+#         # We do not clamp to the boundary here as it may degrade the performance of certain algorithms.
+#         # one should use `cache_size=1` instead
+#         return self.atanh(y)
+#
+#     def log_abs_det_jacobian(self, x, y):
+#         # We use a formula that is more numerically stable, see details in the following link
+#         # https://github.com/tensorflow/probability/commit/ef6bb176e0ebd1cf6e25c6b5cecdd2428c22963f#diff-e120f70e92e6741bca649f04fcd907b7
+#         return 2. * (math.log(2.) - x - F.softplus(-2. * x))
+#
+# class SquashedNormal(pyd.transformed_distribution.TransformedDistribution):
+#     def __init__(self, loc, scale):
+#         self.loc = loc
+#         self.scale = scale
+#
+#         self.base_dist = pyd.Normal(loc, scale)
+#         transforms = [TanhTransform()]
+#         super().__init__(self.base_dist, transforms)
+#
+#     @property
+#     def mean(self):
+#         mu = self.loc
+#         for tr in self.transforms:
+#             mu = tr(mu)
+#         return mu
 
 
 
-
-
-
-
-def load_checkpoint(path):
-    raise NotImplementedError
+# def load_checkpoint(path):
+#     raise NotImplementedError
